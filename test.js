8e75e7d689fe96ba364984d642817ab7e794815d
Franklin van de Meent <fr@nkl.in>
2016-03-16 03:45:39 +0100

    Test: replaced inline try..catch with function

--- test.js
+++ test.js
@@ -227,6 +227,14 @@ queue.push (function () {
 });
 
 // method
+function test_info (data) {
+  try {
+    return data.entries [0].data.Pressure_surface;
+  } catch (e) {
+    return e;
+  }
+}
+
 queue.push (function () {
   var params = {
     lon: -50.5,
@@ -240,13 +248,7 @@ queue.push (function () {
       ['fail', 'type', data instanceof Object, true],
       ['warn', '.stats', data && data.stats instanceof Object, true],
       ['warn', '.entries', data && data.entries instanceof Array, true],
-      ['info', 'Pressure_surface',
-        try {
-          return data.entries [0].data.Pressure_surface;
-        } catch (e) {
-          return e;
-        }
-      ]
+      ['info', 'Pressure_surface', test_info (data)]
     ]);
   });
 });

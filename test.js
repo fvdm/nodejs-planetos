9ff653c8f8af5fb9e6880485edef5f4c29b3a4a4
Franklin van de Meent <fr@nkl.in>
2016-03-18 05:38:45 +0100

    Test: switched to dotest dep

--- test.js
+++ test.js
@@ -5,17 +5,10 @@ Feedback:       https://github.com/fvdm/nodejs-planetos/issues
 License:        Unlicense (public domain)
 */
 
-var path = require ('path');
-var dir = path.dirname (module.filename);
-
-var pkg = require (path.join (dir, 'package.json'));
-var app = require (path.join (dir));
+var doTest = require ('dotest');
+var app = require ('./');
 
 var planetos;
-var errors = 0;
-var warnings = 0;
-var queue = [];
-var next = -1;
 
 
 // Setup
@@ -26,221 +19,32 @@ var config = {
   timeout: process.env.PLANETOS_TIMEOUT || 5000
 };
 
-if (!config.apikey) {
-  config.endpoint = 'https://frankl.in/u/ci_test.php?a=planetos&b=';
-}
-
 planetos = app (config);
 
 
-/**
- * ANSI colorize a string
- *
- * @param color {String} - The color to add
- * @param str {String} - The string to alter
- * @returns {String}
- */
-
-function colorStr (color, str) {
-  var colors = {
-    red: '\u001b[31m',
-    green: '\u001b[32m',
-    yellow: '\u001b[33m',
-    blue: '\u001b[34m',
-    magenta: '\u001b[35m',
-    cyan: '\u001b[36m',
-    gray: '\u001b[37m',
-    bold: '\u001b[1m',
-    plain: '\u001b[0m'
-  };
-
-  return colors [color] + str + colors.plain;
-}
-
-
-/**
- * console.log with style
- *
- * @param [type] {String=plain} - Formatting style
- * @param str {String} - The string to alter
- * @returns {void}
- */
-
-function log (type, str) {
-  var types = {
-    fail: ['red', 'FAIL'],
-    good: ['green', 'good'],
-    warn: ['yellow', 'warn'],
-    info: ['cyan', 'info']
-  };
-
-  if (!str) {
-    str = type;
-    type = 'plain';
-  }
-
-  switch (type) {
-    case 'error': console.log (colorStr ('red', colorStr ('bold', 'ERROR   ')) + str + '\n'); break;
-    case 'note': console.log (colorStr ('bold', str)); break;
-    case 'plain': console.log (str); break;
-    default:
-      console.log (colorStr (types[type][0], types[type][1]) + '    ' + str);
-      break;
-  }
-}
-
-
-/**
- * Detect and wrap string type
- *
- * @param str {String} - The string
- * @returns {String}
- */
-
-function typeStr (str) {
-  if (typeof str === 'string') {
-    str = '"' + str + '"';
-  } else if (str instanceof Object) {
-    str = 'Object';
-  } else if (str instanceof Array) {
-    str = 'Array';
-  } else if (str instanceof Error) {
-    str = 'Error';
-  }
-
-  return colorStr ('magenta', str);
-}
-
-// handle exits
-process.on ('exit', function processExit () {
-  console.log ();
-  log ('info', errors + ' errors');
-  log ('info', warnings + ' warnings');
-  console.log ();
-
-  if (errors) {
-    process.exit (1);
-  } else {
-    process.exit (0);
-  }
-});
-
-// prevent errors from killing the process
-process.on ('uncaughtException', function uncaughtException (err) {
-  console.log (err);
-  console.log ();
-  console.log (err.stack);
-  console.log ();
-  errors++;
-});
-
-
-/**
- * Queue to prevent flooding
- *
- * @returns {void}
- */
-
-function doNext () {
-  next++;
-  if (queue [next]) {
-    console.log ();
-    queue [next] ();
-  }
-}
-
-
-/**
- * doTest checks for error
- * else runs specified tests
- *
- * @param {Error} err
- * @param {String} label
- * @param {Array} tests
- * @returns {void}
- *
- * doTest(err, 'label text', [
- *   ['fail', 'feeds', typeof feeds, 'object'],
- *   ['warn', 'music', music instanceof Array, true],
- *   ['info', 'tracks', music.length]
- * ]);
- */
-
-function doTest (err, label, tests) {
-  var level = 'good';
-  var test;
-  var i;
-
-  if (err instanceof Error) {
-    log ('error', label);
-    console.dir (err, { depth: null, colors: true });
-    console.log ();
-    console.log (err.stack);
-    console.log ();
-    errors++;
-
-    doNext ();
-    return;
-  }
-
-  console.log (colorStr ('blue', '(' + (next + 1) + '/' + queue.length + ') ') + colorStr ('bold', label));
-
-  for (i = 0; i < tests.length; i++) {
-    test = {
-      level: tests [i] [0],
-      label: tests [i] [1],
-      result: tests [i] [2],
-      expect: tests [i] [3]
-    };
-
-    if (test.result === test.expect) {
-      log ('good', colorStr ('blue', test.label) + ': ' + typeStr (test.result) + ' is exactly ' + typeStr (test.expect));
-    }
-
-    if (test.level === 'fail' && test.result !== test.expect) {
-      errors++;
-      level = 'fail';
-      log ('fail', colorStr ('blue', test.label) + ': ' + typeStr (test.result) + ' is not ' + typeStr (test.expect));
-    }
-
-    if (test.level === 'warn' && test.result !== test.expect) {
-      warnings++;
-      level = level !== 'fail' && 'warn';
-      log ('warn', colorStr ('blue', test.label) + ': ' + typeStr (test.result) + ' is not ' + typeStr (test.expect));
-    }
-
-    if (test.level === 'info') {
-      log ('info', colorStr ('blue', test.label) + ': ' + typeStr (test.result));
-    }
-  }
-
-  doNext ();
-}
-
-
 // module basics
-queue.push (function () {
-  doTest (null, 'Module', [
-    ['fail', 'exports', typeof app, 'function'],
-    ['fail', 'interface', typeof planetos, 'function']
-  ]);
+doTest.add ('Module', function () {
+  doTest.test ()
+    .isFunction ('fail', 'app', app)
+    .isFunction ('fail', 'planetos', planetos)
+    .done ();
 });
 
 // error
-queue.push (function () {
+doTest.add ('API error', function () {
   planetos ('noaa_ww3_global_1.25x1d', {}, function (err, data) {
-    doTest (null, 'API error', [
-      ['fail', 'type', err instanceof Error, true],
-      ['fail', 'data', !data, true],
-      ['fail', '.message', err && err.message, 'API error'],
-      ['warn', '.statusCode', err && err.statusCode !== 200, true],
-      ['warn', '.body', err && typeof err.body, 'string']
-    ]);
+    doTest.test ()
+      .isError ('fail', 'err', err)
+      .isUndefined ('fail', 'data', data)
+      .isExactly ('fail', 'err.message', err && err.message, 'API error')
+      .isExactly ('warn', 'err.statusCode', err && err.statusCode, 400)
+      .isNotEmpty ('warn', 'err.body', err && err.body)
+      .done ();
   });
 });
 
 // method
-queue.push (function () {
+doTest.add ('NOAA example', function () {
   var params = {
     lon: -50.5,
     lat: 49.5,
@@ -249,15 +53,15 @@ queue.push (function () {
   };
 
   planetos ('noaa_ww3_global_1.25x1d', params, function (err, data) {
-    doTest (err, 'NOAA example', [
-      ['fail', 'type', data instanceof Object, true],
-      ['warn', '.stats', data && data.stats instanceof Object, true],
-      ['warn', '.entries', data && data.entries instanceof Array, true]
-    ]);
+    doTest.test (err)
+      .isObject ('fail', 'data', data)
+      .isObject ('fail', 'data.stats', data && data.stats)
+      .isArray ('fail', 'data.entries', data && data.entries)
+      .done ();
 
     if (data) {
       console.log ();
-      log ('note', 'JSON data:');
+      doTest.log ('note', 'JSON data:');
       console.dir (data, {
         depth: null,
         colors: true
@@ -268,9 +72,4 @@ queue.push (function () {
 
 
 // Start the tests
-log ('note', 'Running tests...\n');
-log ('note', 'API endpoint:  ' + (config.endpoint || 'default'));
-log ('note', 'Node.js:       ' + process.versions.node);
-log ('note', 'Module:        ' + pkg.version);
-
-doNext ();
+doTest.run ();
